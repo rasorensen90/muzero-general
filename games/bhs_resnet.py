c036eff9a3409ec3f96aa37e0525403a0fa565ad
Sørensen <Rene.Arendt.Soerensen@beumer.com>
2020-11-12 11:49:04 +0100

    Update bhs_resnet.py

--- games/bhs_resnet.py
+++ games/bhs_resnet.py
@@ -32,7 +32,7 @@ class MuZeroConfig:
 
 
         ### Self-Play
-        self.num_workers = 30  # Number of simultaneous threads/workers self-playing to feed the replay buffer
+        self.num_workers = 10  # Number of simultaneous threads/workers self-playing to feed the replay buffer
         self.selfplay_on_gpu = False
         self.max_moves = 200  # Maximum number of moves if game is not finished before
         self.num_simulations = 20  # Number of future moves self-simulated
