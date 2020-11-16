afb64266c2374e82fd6ef5b9c32dda945e59f236
Sørensen <Rene.Arendt.Soerensen@beumer.com>
2020-11-16 15:59:05 +0100

    Update factory_fully_conn.py

--- games/factory_fully_conn.py
+++ games/factory_fully_conn.py
@@ -99,7 +99,7 @@ class MuZeroConfig:
 
         ### Replay Buffer
         self.replay_buffer_size = 1000  # Number of self-play games to keep in the replay buffer
-        self.num_unroll_steps = 10  # Number of game moves to keep for every batch element
+        self.num_unroll_steps = 500  # Number of game moves to keep for every batch element
         self.td_steps = 500  # Number of steps in the future to take into account for calculating the target value
         self.PER = True  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
         self.PER_alpha = 1.  # How much prioritization is used, 0 corresponding to the uniform case, paper suggests 1
