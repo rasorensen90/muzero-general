3c4ec1fa4a6972edd36d1087f98161636ea078a1
Sørensen <Rene.Arendt.Soerensen@beumer.com>
2020-11-19 22:27:06 +0100

    Update factory_fully_conn.py

--- games/factory_fully_conn.py
+++ games/factory_fully_conn.py
@@ -35,7 +35,7 @@ class MuZeroConfig:
 
 
         ### Self-Play
-        self.num_workers = 30  # Number of simultaneous threads/workers self-playing to feed the replay buffer
+        self.num_workers = 1  # Number of simultaneous threads/workers self-playing to feed the replay buffer
         self.selfplay_on_gpu = False
         self.max_moves = 50000  # Maximum number of moves if game is not finished before
         self.num_simulations = 20  # Number of future moves self-simulated
